d0d18e9f38fca6e5ed480bb869bb156d251ce702
Ben Ely <ben.ely@h4bmanchester.com>
2018-12-04 14:21:57 +0000

    Complete task for day 2

--- src/day-1.js
+++ src/day-1.js
@@ -1,7 +1,27 @@
-const calibration = function (frequencies) {
-    return frequencies.split(',').reduce((accumulator, frequency) => {
-        return accumulator + parseInt(frequency.trim())
-    }, 0)
+export const calibration = function (input) {
+    return input
+        .split(',')
+        .reduce((accumulator, frequency) =>  accumulator + parseInt(frequency.trim()), 0);
 }
 
-export default calibration
+export const recalibration = function (input) {
+    const frequencies = input.split(',').map(frequency => parseInt(frequency.trim()));
+    
+    let i = 0;
+    let pastTotals = [];
+    let runningTotal = 0;
+
+    while (!pastTotals.includes(runningTotal)) {
+        pastTotals.push(runningTotal);
+        
+        runningTotal += frequencies[i];
+
+        i++;
+        
+        if (i === frequencies.length) {
+            i = 0;
+        }
+    }
+
+    return runningTotal;
+}
